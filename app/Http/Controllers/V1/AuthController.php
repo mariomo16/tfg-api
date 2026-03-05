162fddc15d0415093d6ff6020aa83e81235f84b6
mariomo16 <moralesortegamario02@gmail.com>
2026-03-05 18:53:50 +0100

    Create AuthController scaffold

---
+++ app/Http/Controllers/V1/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        //
+    }
+
+    public function logout(Request $request)
+    {
+        //
+    }
+
+    public function user(Request $request)
+    {
+        //
+    }
+}
