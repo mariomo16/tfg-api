b9e71f2c3e0bb32cf0e3d70949910f5bd368bad8
mariomo16 <mariomo16.cieza@gmail.com>
2026-03-06 15:11:49 +0100

    Modified status route to send more important information to the frontend

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/status', function () {
+// Para el componente health de Angular
+Route::get('/up', function () {
     return response()->json([
-        'status' => 'ok',
-        'timestamp' => now()
+        'status' => 'up',
+        'time' => round((microtime(true) - LARAVEL_START) * 1000),
     ]);
 });
 
