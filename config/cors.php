8e41aa9402ba7e4c8afff8ef517f51e62f1fc7dc
mariomo16 <moralesortegamario02@gmail.com>
2026-03-09 13:55:03 +0100

    Allow vercel web app domain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
   'allowed_methods' => ['*'],
 
-  'allowed_origins' => ['http://localhost:4200', 'https://ortegamario-tfg.vercel.app'],
+  'allowed_origins' => ['http://localhost:4200', 'ortegamario-tfg-web.vercel.app'],
 
   'allowed_origins_patterns' => [],
 
